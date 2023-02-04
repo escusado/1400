f218931e2d7b5d7f6d23fc949febddda860f845a
escusado <escusado@gmail.com>
2023-02-04 10:22:03 -0600

    adds basic framework

---
+++ src/components/main-stage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const MainStage: FC = () => {
+  return <div className="m-0 h-full w-full bg-amber-200 p-0">MainStage</div>
+}
+
+export default MainStage
---
+++ src/pages/main-game.tsx
@@ -0,0 +1,8 @@
+import MainStage from "@/components/main-stage";
+import { FC } from "react";
+
+const MainGamePage: FC= () => {
+  return <MainStage />;
+};
+
+export default MainGamePage;
