671c8ac6ade2fa3159ae3dc9c37d5e857ea292ed
Hans Ramírez <hans@bekind.software>
2023-02-04 14:32:59 -0600

    feat(cardselection): Added logic for card selection and display

--- src/components/player-corner.tsx
+++ src/components/player-corner.tsx
@@ -5,10 +5,21 @@ type PlayerCornerProps = {
   className?: string
 }
 
+// pokerhandArray 
+
+
+
 const PlayerCorner: FC<PlayerCornerProps> = ({ className }) => {
+
+
+
   return (
-    <div className={`w-full bg-sky-900 p-8  ${className}`}>
-      <PlayerHandLeft />
+    <div className={`w-full bg-[url('/placeholders/hero_bg.png')] bg-cover h-2/5   ${className}`}>
+      <div className='flex items-center align-middle  h-full w-full'>
+        <div className='ml-20'>
+          <PlayerHandLeft />
+        </div>
+      </div>
     </div>
   )
 }
