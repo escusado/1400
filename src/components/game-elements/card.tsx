917f6fd1b839436c53d1be2a333e47fad8c1ce2a
escusado <escusado@gmail.com>
2023-02-04 20:52:10 -0600

    adds shadow style

--- src/components/game-elements/card.tsx
+++ src/components/game-elements/card.tsx
@@ -27,20 +27,22 @@ const Card: FC<CardProps> = ({
   flipped = true,
 }) => {
   const css = classNames(
-    "aspect-[4/5] max-w-[150px]",
+    "aspect-[4/5] max-w-[150px] transition-all drop-shadow-md drop-shadow-[0_5px_5px_rgba(0,0,0,0.5)]",
     className,
     isInteractive && "hover:transition-transform hover:-translate-y-5",
-    selected && "border-4  border-yellow-500",
+    selected && "-translate-y-8 brightness-150",
   );
   return (
     <div className={css} onClick={() => isInteractive && selectCard(value)}>
       <div
         className={classNames(
-          "h-full cursor-pointer bg-white  text-gray-800 hover:shadow-lg",
+          "h-full cursor-pointer text-gray-800 hover:shadow-lg",
         )}
       >
-        {/* <img className="object-cover h-full w-full" src="/placeholders/ace.png" /> */}
-        <p>{value}</p>
+        <img
+          className="h-full w-full object-cover"
+          src={`/deck/${value}.png`}
+        />
       </div>
     </div>
   );
